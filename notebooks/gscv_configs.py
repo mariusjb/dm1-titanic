896ceccea586c01f979a075fa757550c614a4fac
Marius J Bauer <bauermarius@icloud.com>
2022-11-16 09:55:18 +0100

    add gradient boosting

--- notebooks/gscv_configs.py
+++ notebooks/gscv_configs.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
 
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.model_selection import KFold
+from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
 
 
@@ -38,6 +39,31 @@ gscv_cfg_rf = GSCVConfig(
         "min_samples_leaf": np.arange(1, 4, 1),
         "max_features": ["sqrt", "log2", None],
         "bootstrap": [True, False],
+    },
+    KFold(n_splits=10, shuffle=True),
+)
+
+gscv_cfg_gb = GSCVConfig(
+    "gradient_boosting",
+    GradientBoostingClassifier(),
+    {
+        "learning_rate": [0.001, 0.01, 0.1],
+        "n_estimators": [10, 50, 100],
+        "max_depth": [1, 3, 7],
+        "max_features": ['sqrt', 'log2'],
+    },
+    KFold(n_splits=10, shuffle=True),
+)
+
+gscv_cfg_mlp = GSCVConfig(
+    "multilayer_perceptron",
+    MLPClassifier(),
+    {
+        "hidden_layer_sizes": [(50,), (100,), (500,)],
+        "activation": ["identity", "logistic", "tanh", "relu"],
+        "solver": ["lbfgs", "sgd", "adam"],
+        "batch_size": ["auto"],
+        "learning_rate": ["constant", "invscaling", "adaptive"],
     },
     KFold(n_splits=10, shuffle=True),
 )
