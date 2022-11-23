4ac8f71b737e1c399a5f973d89c8a52a9cf660d9
Marius J Bauer <bauermarius@icloud.com>
2022-11-23 18:04:08 +0100

    finetuning mlp

--- notebooks/gscv_configs.py
+++ notebooks/gscv_configs.py
@@ -21,11 +21,11 @@ gscv_cfg_dt = GSCVConfig(
     "decision_tree",
     DecisionTreeClassifier(),
     {
-        "criterion": ["gini", "entropy", "log_loss"],
-        "max_depth": [5, 25, 50, None],
-        "min_samples_split": [2, 3, 4, 5],
-        "min_samples_leaf": [1, 2, 3, 4],
-        "max_features": ["sqrt", "log2", None],
+        "criterion": ["log_loss"],
+        "max_depth": [5, 10, 15],
+        "min_samples_split": [2],
+        "min_samples_leaf": [1],
+        "max_features": ["sqrt"],
     },
     StratifiedKFold(n_splits=10, shuffle=True, random_state=0),
 )
@@ -34,7 +34,7 @@ gscv_cfg_rf = GSCVConfig(
     "random_forest",
     RandomForestClassifier(),
     {
-        "n_estimators": [10, 20],
+        "n_estimators": [18, 20, 22],
         "criterion": ["entropy"],
         "max_depth": [None],
         "min_samples_split": [5, 7],
@@ -49,10 +49,10 @@ gscv_cfg_gb = GSCVConfig(
     "gradient_boosting",
     GradientBoostingClassifier(),
     {
-        "learning_rate": [0.001, 0.01, 0.1],
-        "n_estimators": [10, 50, 100],
-        "max_depth": [1, 3, 7],
-        "max_features": ['sqrt', 'log2'],
+        "learning_rate": [0.25, 0.3, 0.35],
+        "n_estimators": [23, 25, 27],
+        "max_depth": [8],
+        "max_features": ['log2'],
     },
     StratifiedKFold(n_splits=10, shuffle=True, random_state=0),
 )
@@ -61,12 +61,12 @@ gscv_cfg_mlp = GSCVConfig(
     "multilayer_perceptron",
     MLPClassifier(),
     {
-        "hidden_layer_sizes": [(10,), (25,), (75,)],
-        "activation": ["identity", "logistic", "tanh", "relu"],
-        "solver": ["lbfgs", "sgd", "adam"],
+        "hidden_layer_sizes": [(10,), (11,), (12,)],
+        "activation": ["relu"],
+        "solver": ["adam"],
         "batch_size": ["auto"],
-        "learning_rate": ["constant", "invscaling", "adaptive"],
-        "max_iter": [1000],
+        "learning_rate": ["invscaling"],
+        "max_iter": [1400, 1500],
     },
     StratifiedKFold(n_splits=10, shuffle=True, random_state=0),
 )
@@ -75,9 +75,9 @@ gscv_cfg_knn = GSCVConfig(
     "knearest_neighbor",
     KNeighborsClassifier(),
     {
-        "n_neighbors": [3, 5, 10],
-        "weights": ["uniform", "distance"],
-        "algorithm": ["auto", "ball_tree", "kd_tree", "brute"],
+        "n_neighbors": [9, 10, 11],
+        "weights": ["distance"],
+        "algorithm": ["ball_tree"],
     },
     StratifiedKFold(n_splits=10, shuffle=True, random_state=0),
 )
