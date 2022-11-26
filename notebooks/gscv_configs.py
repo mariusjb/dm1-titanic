22446d0f2814a94ea1d62de8e9f2241b1030c5d7
Marius J Bauer <bauermarius@icloud.com>
2022-11-26 17:09:59 +0100

    further results

--- notebooks/gscv_configs.py
+++ notebooks/gscv_configs.py
@@ -8,6 +8,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
+from sklearn.linear_model import LogisticRegression
 
 
 @dataclass
@@ -22,12 +23,23 @@ gscv_cfg_dt = GSCVConfig(
     "decision_tree",
     DecisionTreeClassifier(),
     {
-        "criterion": ["log_loss"],
-        "max_depth": [5, 10, 15],
-        "min_samples_split": [2],
-        "min_samples_leaf": [1],
-        "max_features": ["sqrt"],
+        'class_weight': [None],
+        'criterion': ['log_loss'],
+        'max_features': [10],
+        'min_impurity_decrease': [0.01],
+        'min_samples_leaf': [9],
+        'min_samples_split': [15],
+        'splitter': ['best']
     },
+    # {
+    #     'class_weight': [None],
+    #     'criterion': ['log_loss'],
+    #     'max_features': [10],
+    #     'min_impurity_decrease': [0.01],
+    #     'min_samples_leaf': [9],
+    #     'min_samples_split': [15],
+    #     'splitter': ['best']
+    # },
     StratifiedKFold(n_splits=10, shuffle=True, random_state=0),
 )
 
@@ -35,13 +47,21 @@ gscv_cfg_rf = GSCVConfig(
     "random_forest",
     RandomForestClassifier(),
     {
-        "n_estimators": [32, 64, 128],
-        "criterion": ["gini", "entropy", "log_loss"],
-        "max_depth": [2, 3, 4],
-        "min_samples_split": [0.1, 0.2, 0.3, 0.4],
-        "min_samples_leaf": [0.05, 0.1, 0.15],
-        "max_features": ["sqrt", "log2", None],
-        "bootstrap": [True, False]
+        "bootstrap": [False],
+        "criterion": ["entropy"],
+        "max_depth": [None],
+        "max_features": ["log2"],
+        "min_samples_leaf": [2],
+        "min_samples_split": [5],
+        "n_estimators": [10]
+
+        # "n_estimators": [32, 64, 128],
+        # "criterion": ["gini", "entropy", "log_loss"],
+        # "max_depth": [2, 3, 4],
+        # "min_samples_split": [0.1, 0.2, 0.3, 0.4],
+        # "min_samples_leaf": [0.05, 0.1, 0.15],
+        # "max_features": ["sqrt", "log2", None],
+        # "bootstrap": [True, False]
     },
     StratifiedKFold(n_splits=10, shuffle=True, random_state=0),
 )
@@ -104,12 +124,67 @@ gscv_cfg_nb = GSCVConfig(
 gscv_cfg_xgb = GSCVConfig(
     "xgboost",
     XGBClassifier(),
+
+    # chiaras params
+    # v1
+    # {
+    #     "learning_rate": [0.15],
+    #     "subsample": [0.7],
+    #     "max_depth": [1],
+    #     "colsample_bylevel": [0.4],
+    #     "colsample_bytree": [0.5],
+    #     "n_estimators": [100],
+    # },
+
+    # v2
+    # {
+    #     "learning_rate": [0.15],
+    #     "subsample": [0.9],
+    #     "max_depth": [4],
+    #     "colsample_bylevel": [0.4],
+    #     "colsample_bytree": [0.15],
+    #     "n_estimators": [30],
+    #     "gamma": [1]
+    # },
+
+    # v3
+    # {
+    #     "learning_rate": [0.15],
+    #     "subsample": [0.6],
+    #     "max_depth": [3],
+    #     "colsample_bylevel": [0.2],
+    #     "colsample_bytree": [0.9],
+    #     "n_estimators": [20],
+    #     "gamma": [1]
+    # },
+
+    # v4
     {
-        "learning_rate": [0.01],
-        "max_depth": [8, 9],
-        "colsample_bytree": [0.55, 0.6, 0.65],
-        "n_estimators": [20, 25, 30],
-        "gamma": [0.45, 0.5, 0.55],
+        "learning_rate": [0.7],
+        "subsample": [0.6],
+        "max_depth": [3],
+        "colsample_bylevel": [0.2],
+        "colsample_bytree": [0.9],
+        "n_estimators": [100],
+        "gamma": [0]
     },
+
+    
+    # my params
+    # {
+    #     "learning_rate": [0.01],
+    #     "max_depth": [8, 9],
+    #     "colsample_bytree": [0.55, 0.6, 0.65],
+    #     "n_estimators": [20, 25, 30],
+    #     "gamma": [0.45, 0.5, 0.55],
+    # },
     StratifiedKFold(n_splits=10, shuffle=True, random_state=0),
 )
+
+gscv_cfg_lr = GSCVConfig(
+    "logistic_regression",
+    LogisticRegression(),
+    {},
+    StratifiedKFold(n_splits=10, shuffle=True, random_state=0),
+)
+
